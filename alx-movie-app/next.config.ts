ef6488bff77f636ccc349f214f176af37e580398
Ricokamau <ricokamau@gmail.com>
2025-08-11 12:37:01 +0300

    Task 3: Add movies page, API route, and related components

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'], // ✅ Allow Amazon images
+  },
 };
 
 export default nextConfig;
